2a75fa092c8a112a569a6a564f6e9fb961398efb
senthilarul <senthilhariharana@gmail.com>
2017-09-24 16:16:58 -0700

    added gtest function

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,33 @@
-#include <gtest/gtest.h>
+/**
+ * @file    test.cpp
+ * @author  Senthil Hariharan Arul
+ * @copyright GNU Public License
+ * @brief Defines test to check mothods of class PIDController.
+ */
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include <gtest/gtest.h>
+#include <PIDController.hpp>
+#include <memory>
+
+/**
+ *@brief Test if the gain constants are stored correctly. 
+ */
+TEST(test, checkconstantvalues) {
+    PIDController PID(0.01, 0.01, 0.01, 1);
+    ASSERT_FLOAT_EQ(PID.getProportional(), 0.01);
+    ASSERT_FLOAT_EQ(PID.getIntegral(), 0.01);
+    ASSERT_FLOAT_EQ(PID.getDerivative(), 0.01);
 }
+
+/**
+ *@brief Test if the newVelocity after PID converges to the setpoint within
+  given threshold limit. 
+ */
+TEST(test1, checkconvergance) {
+    PIDController PID(0.01, 0.01, 0.01, 0.1);
+    EXPECT_NEAR(PID.tuneController(), 1, 0.00001);
+}
+
+
+
+
