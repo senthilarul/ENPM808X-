dfc74eac2d95e9b2b2a3df1ecdced201b6448df0
senthilarul <senthilhariharana@gmail.com>
2017-09-24 16:09:15 -0700

    Modified for calling PID class

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
-#include <lib.hpp>
+/**
+ * @file    main.cpp
+ * @author  Senthil Hariharan Arul
+ * @copyright GNU Public License
+ * @brief Calls the function to tune controller.
+ */
 
-int main()
-{
-    dummy();
-    return 0;
+#include <iostream>
+#include <memory>
+#include "PIDController.hpp"
+
+int main() {
+    std::shared_ptr<PIDController>PID;
+    PID = std::make_shared<PIDController>(0.01, 0.01, 0.01, 0.1);
+    PID->tuneController();
 }
